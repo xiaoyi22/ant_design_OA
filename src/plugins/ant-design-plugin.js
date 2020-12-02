c76aaf5b594e3a3f67eb9a650e46c6dbf86d50b3
Meet-PC\xiaoyi <2044948322@qq.com>
2020-12-02 17:37:02 +0800

    添加登录界面ui

---
+++ src/plugins/ant-design-plugin.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import {
+    Avatar,
+    Form,
+    FormModel,
+    Input,
+    InputNumber,
+    Layout,
+    Row,
+    Col,
+    Button,
+    Icon,
+    Radio,
+    Checkbox
+} from "ant-design-vue"
+
+Vue.use(Avatar)
+Vue.use(Form)
+Vue.use(FormModel)
+Vue.use(Input)
+Vue.use(InputNumber)
+Vue.use(Layout)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Button)
+Vue.use(Icon)
+Vue.use(Radio)
+Vue.use(Checkbox)
---
+++ src/views/Userlogin.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="index_content">
+    <div class="login-panel-logo">
+      <img src="../assets/logo (2).png" alt="" />
+    </div>
+    <div class="login-panel">
+      <span>登录</span>
+      <a-form>
+        <a-form-item>
+          <a-input placeholder="username">
+            <a-icon
+              slot="prefix"
+              type="user"
+              style="color: rgba(0, 0, 0, 0.25)"
+            >
+            </a-icon>
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-input-password placeholder="password">
+            <a-icon
+              slot="prefix"
+              type="lock"
+              style="color: rgba(0, 0, 0, 0.25)"
+            ></a-icon>
+          </a-input-password>
+        </a-form-item>
+        <a-form-item>
+          <a-checkbox>7天内保持登录</a-checkbox>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary">登录</a-button>
+        </a-form-item>
+      </a-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Userlogin",
+  data() {
+    return {};
+  },
+  mounted() {
+    // this.Viewportheight = document.body.clientHeight;
+    // console.log(this.Viewportheight)
+  },
+};
+</script>
+
+<style lang="less">
+.index_content {
+  background: url("../assets/1b4f5529-fc5d-4f3f-950f-30ba6558c89d.png");
+  width: 100%;
+  height: 100%;
+  background-size: 100% 100%;
+  position: fixed;
+  .login-panel-logo {
+    margin-top: 100px;
+    img {
+      width: 200px;
+    }
+  }
+  .login-panel {
+    margin: 20px auto;
+    background: #f3f3f3;
+    width: 336px;
+    .ant-form{
+      margin: 0 auto;
+      width: 80%;
+    }
+    .ant-input {
+      margin:0 auto;
+      height: 40px;
+    }
+    .ant-input-suffix {
+      left: 240px;
+    }
+  }
+}
+</style>
